b390a9ecfdbc0e3cf76a01f11552f36908bb14ec
VanshComp <vanshgautam2005@gmail.com>
2024-03-10 05:58:34 +0530

    Stopwatch with current time

--- stopwatch.py
+++ stopwatch.py
@@ -4,8 +4,9 @@ def time_converter(sec):
     min=sec//60
     hour=min//60
     min=sec%60
-    print("Time covered is :: {%d:%d:%d}"%(int(hour),int(min),int(sec)))
+    print("\n\nTime covered is :: {%d:%d:%d}\n\n"%(int(hour),int(min),int(sec)))
 initial_time=input("Press Enter button to start: ")
+print("\n\nStopwatch starting time is: %s\n\n"%(time.strftime("%H:%M:%S")))
 initial_time=time.time()    
 while True:
     choice=input("break or not??")
@@ -14,10 +15,12 @@ while True:
         break_time=time.time()
         time_break=initial_time-break_time
         time_converter(time_break)
+        print("\n\nStopwatch halting time is: %s\n\n"%(time.strftime("%H:%M:%S")))
         continue
     else:    
         final_time=input("Press stop button to end: ")
         final_time=time.time()
         time_covered=initial_time-final_time 
         time_converter(time_covered)
+        print("\n\nStopwatch ending time is: %s\n\n"%(time.strftime("%H:%M:%S")))
         break    
